a22d6f2bdc59dff472d9c46e9d3f93daeed45d0f
AkshayG <akshayg14@gmail.com>
2020-09-08 00:42:17 +0530

    Updated the notification display

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('JS loaded!!!');
-
 const wordElement = document.getElementById('word');
 const wrongLettersElement = document.getElementById('wrong-letters');
 const playAgainButton = document.getElementById('play-again-button');
@@ -38,5 +36,39 @@ function displayWord() {
     popup.style.display = 'flex';
   }
 }
+
+// Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Update the wrong letters
+function updateWrongLettersElement() { }
+
+// Keydown letter press
+window.addEventListener('keydown', event => {
+  if (event.keyCode >= 65 && event.keyCode <= 90) {
+    const letterInput = event.key;
+    if (selectedWord.includes(letterInput)) {
+      if (!correctLetters.includes(letterInput)) {
+        correctLetters.push(letterInput);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letterInput)) {
+        wrongLetters.push(letterInput);
+        updateWrongLettersElement();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
